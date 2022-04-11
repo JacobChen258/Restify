e1da1fc00cea2d0c45e29864ca0b4f1eec3d5923
Litao Chen <55116264+JacobChen258@users.noreply.github.com>
2022-04-11 14:44:44 -0400

    update all endpoints

--- backend/accounts/views/follow.py
+++ backend/accounts/views/follow.py
@@ -10,7 +10,7 @@ from rest_framework.generics import DestroyAPIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.status import HTTP_400_BAD_REQUEST
-
+from notifications.serializers import NotificationsSerializer
 class Follow(CreateAPIView,DestroyAPIView):
     serializer_class = FollowedRestaurantSerializer
     permission_classes = [IsAuthenticated,]
@@ -18,11 +18,11 @@ class Follow(CreateAPIView,DestroyAPIView):
     def post(self, request, *args, **kwargs):
 
         if "restaurant" in request.data:
-            get_object_or_404(Restaurant,id=request.data["restaurant"])
+            self.object = get_object_or_404(Restaurant,id=request.data["restaurant"])
 
             follow = FollowedRestaurant.objects.filter(user=request.user.id, restaurant=request.data['restaurant'])
             if follow.exists():
-                return Response(status=status.HTTP_201_CREATED)
+                return Response(status=status.HTTP_409_CONFLICT)
             return self.create(request, args, kwargs)
 
         return Response(status=HTTP_400_BAD_REQUEST)
@@ -33,6 +33,9 @@ class Follow(CreateAPIView,DestroyAPIView):
         serializer = self.get_serializer(data=data)
         serializer.is_valid(raise_exception=True)
         self.perform_create(serializer)
+        self.notify_owner()
+        self.object.num_followers += 1
+        self.object.save()
         headers = self.get_success_headers(serializer.data)
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
@@ -41,5 +44,18 @@ class Follow(CreateAPIView,DestroyAPIView):
             return Response(status=HTTP_400_BAD_REQUEST)
         return super().delete(request, *args, **kwargs)
 
+    def perform_destroy(self, instance):
+        restaurant = get_object_or_404(Restaurant,id = self.request.data['restaurant'])
+        restaurant.num_followers -= 1
+        restaurant.save()
+        return super().perform_destroy(instance)
+
     def get_object(self):
         return get_object_or_404(FollowedRestaurant, user=self.request.user.id, restaurant=self.request.data['restaurant'])
+
+    def notify_owner(self):
+        content = f"User {self.request.user.first_name} {self.request.user.last_name} just followed your restaurant."
+        data = {'viewer':self.object.owner,'content':content}
+        notif_serializer = NotificationsSerializer(data=data)
+        notif_serializer.is_valid(raise_exception=True)
+        notif_serializer.save()
--- backend/restaurants/serializers.py
+++ backend/restaurants/serializers.py
@@ -1,23 +1,43 @@
-from rest_framework import serializers
-from restaurants.models import Restaurant
+from rest_framework.serializers import ModelSerializer,ValidationError
+from restaurants.models import Restaurant,Images,Restaurant_Like
 
-class CreateRestaurantSerializer(serializers.ModelSerializer):
+class CreateRestaurantSerializer(ModelSerializer):
     class Meta:
         model = Restaurant
         fields = ['owner', 'name', 'address', 'logo', 'email', 'postal_code', 'phone_num']
 
-class EditRestaurantSerializer(serializers.ModelSerializer):
+class EditRestaurantSerializer(ModelSerializer):
     class Meta:
         model = Restaurant
         fields = ['name', 'address', 'logo', 'email', 'postal_code', 'phone_num']
 
     def validate(self, request):
         if request.data['owner'] != request.user.id:
-            raise serializers.ValidationError({"Only restaurant owners can edit the restaurant's information"})
+            raise ValidationError({"Only restaurant owners can edit the restaurant's information"})
         return request
 
-class RestaurantInfoSerializer(serializers.ModelSerializer):
+class RestaurantInfoSerializer(ModelSerializer):
 
     class Meta:
         model = Restaurant
         fields = ['id','name', 'address', 'logo', 'email', 'postal_code', 'phone_num', 'num_followers', 'num_likes']
+
+class AddImageSerializer(ModelSerializer):
+    class Meta:
+        model = Images
+        fields = ['restaurant','image']
+
+class GetImageSerializer(ModelSerializer):
+    class Meta:
+        model = Images
+        fields = ['id','image']
+
+class RestaurantSearchSerializer(ModelSerializer):
+    class Meta:
+        model = Restaurant
+        fields = ['id','name']
+
+class LikedRestaurantSerializer(ModelSerializer):
+    class Meta:
+        model = Restaurant_Like
+        fields = ['id', 'user', 'restaurant']
