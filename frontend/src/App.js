071b3db5b7859e653c96a8cf8f9228c5a50ae8b6
stephanmotha <stephan.motha@outlook.com>
2022-04-12 14:52:46 -0400

    add routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,25 @@
 import "./App.css";
 import RestifyNavbar from "./components/Navbar/Navbar";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import PageNotFound from "./components/PageNotFound/PageNotFound";
+import Signup from "./components/Signup/Signup";
+import Login from "./components/Login/Login";
 
 function App() {
-  return <RestifyNavbar />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup/test" element={<RestifyNavbar />}>
+          {/* Components that need navbar go here */}
+          <Route path="/signup/test/2" element={<Signup />} />
+        </Route>
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
