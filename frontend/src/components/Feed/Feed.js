74122dc35701d0b240c3362498485477998bd950
Sahib <sahib.nanda@mail.utoronto.ca>
2022-04-15 19:52:22 -0400

    Added api call to feed

--- frontend/src/components/Feed/Feed.js
+++ frontend/src/components/Feed/Feed.js
@@ -1,25 +1,38 @@
-import React from "react";
+import { React, useState, useEffect } from "react";
 import "./Feed.css";
 import { Col, Row, Container } from "react-bootstrap";
 
 const Feed = () => {
+
+    const [feed, setFeed] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:8000/user/feed/')
+            .then(res => res.json())
+            .then(json => setFeed(json.data))
+    }, [])
+
     return (
         <div className="w-100 bg_color">
-            <section class="bg-secondary text-light py-3">
-                <div class="container">
-                    <div class="row">
-                        <div class="col text-left">
+            <section className="bg-secondary text-light py-3">
+                <div className="container">
+                    <div className="row">
+                        <div className="col text-left">
                             <h2>Your Feed</h2>
                             <br />
-                            <p class="lead">Here you can browse your personal feed to see the blog posts of which restaurants you follow</p>
+                            <p className="lead">Here you can browse your personal feed to see the blog posts of which restaurants you follow</p>
                         </div>
                     </div>
                 </div>     
             </section>  
-            <section class="text-dark p-5">
-                <div class="container">
-                    <div id="blogposts" class="col text-center">
-                        
+            <section className="text-dark p-5">
+                <div className="container">
+                    <div id="blogposts" className="col text-center">
+                        {feed.map(post => (
+                            <div key={post.id} className="row">
+                                <p className="lead">{post.title}</p>
+                            </div>
+                        ))}
                     </div>
                 </div>
             </section>
