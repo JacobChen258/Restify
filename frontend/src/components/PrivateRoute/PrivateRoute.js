8c48e5c4a247f6e76c824ff7f329a8fa0c2a61f6
stephanmotha <stephan.motha@outlook.com>
2022-04-16 00:07:58 -0400

    complete private route

---
+++ frontend/src/components/PrivateRoute/PrivateRoute.js
@@ -0,0 +1,16 @@
+import React, { useContext } from "react";
+import { Navigate } from "react-router-dom";
+import AuthContext from "../Context/AuthContext";
+
+// const useAuth = () => {
+//   return authenticated;
+// };
+function PrivateRoute({ children }) {
+  console.log("hello");
+  console.log(useContext(AuthContext));
+  const { user } = useContext(AuthContext);
+
+  return user ? { children } : <Navigate to="/login" />;
+}
+
+export default PrivateRoute;
