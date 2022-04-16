fa80e355f01899ec05fb382fd2694428cd99abb4
stephanmotha <stephan.motha@outlook.com>
2022-04-16 00:10:55 -0400

    complete signup/login

--- frontend/src/components/Navbar/RestifyNavbar/RestifyNavbar.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import UserNavbar from "../UserNav/Navbar";
-import AnonNavbar from "../AnonNav/Navbar";
-
-const RestifyNavbar = () => {
-  return;
-};
-
-export default RestifyNavbar;
