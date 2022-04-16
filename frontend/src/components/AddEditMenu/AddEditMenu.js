8be735af617e172298f496611e43efcb63572d79
stephanmotha <stephan.motha@outlook.com>
2022-04-16 13:53:26 -0400

    update add menu form

--- frontend/src/components/AddEditMenu/AddEditMenu.js
+++ frontend/src/components/AddEditMenu/AddEditMenu.js
@@ -1,6 +1,61 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import "./AddEditMenu.css";
+import { Card, Button } from "react-bootstrap";
+import axios from "axios";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 
 const AddEditMenu = () => {
+  const nav = useNavigate();
+  const params = useParams();
+  const [menuItems, setmenuItems] = useState({});
+
+  const addMenuItem = () => {
+    axios.post("/user/restaurant/menu/add/", {});
+  };
+
+  const editMenuItems = () => {};
+
+  const getMenuItems = () => {
+    axios
+      .get(`/restaurant/${params.id}/menu/`)
+      .then((res) => setmenuItems(res))
+      .catch((err) => {
+        if (err.response.status == 401) {
+          nav("login");
+        }
+      });
+  };
+
+  const popEdit = () => {};
+
+  const priceRegex = /^(\d{1,8}|\d{0,5}\.\d{1,2})$/;
+  const validation = Yup.object({
+    itemName: Yup.string()
+      .required("Item name is required")
+      .max(20, "Item name is too long"),
+    price: Yup.string()
+      .matches(priceRegex, "Please enter a valid price")
+      .required("Price is required"),
+
+    description: Yup.string()
+      .max(50, "description is too long")
+      .required("Description is required"),
+  });
+
+  const formik = useFormik({
+    initialValues: {
+      itemName: "",
+      price: "",
+      description: "",
+    },
+    validationSchema: validation,
+    onSubmit: (values) => {
+      console.log(values);
+    },
+  });
+
   return (
     <>
       <h1 className="text-left">Your Menu</h1>
@@ -9,110 +64,74 @@ const AddEditMenu = () => {
       <div className="container text-center">
         <div className="row">
           <div className="col">
-            <div className="card" style={{ width: "18rem" }}>
-              <div className="card-body">
-                <h5 className="card-title">Pepperoni Pizza</h5>
-                <h6 className="card-subtitle mb-2 text-muted">$10.99</h6>
-                <p className="card-text">
+            <Card className="menu-item" style={{ width: "18rem" }}>
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Subtitle className="mb-2 text-muted">
+                  Card Subtitle
+                </Card.Subtitle>
+                <Card.Text>
                   Some quick example text to build on the card title and make up
                   the bulk of the card's content.
-                </p>
-                <button
-                  type="submit"
-                  className="btn btn-outline-secondary btn-sm"
+                </Card.Text>
+                <Button
+                  className="ms-1"
+                  onClick={popEdit}
+                  variant="outline-secondary"
                 >
-                  edit
-                </button>
-                <button type="submit" className="btn btn-outline-danger btn-sm">
+                  Edit
+                </Button>
+                <Button className="ms-1" variant="outline-danger">
                   Delete
-                </button>
-              </div>
-            </div>
-          </div>
-          <div className="col">
-            <div className="card" style={{ width: "18rem" }}>
-              <div className="card-body">
-                <h5 className="card-title">Falafel</h5>
-                <h6 className="card-subtitle mb-2 text-muted">$5.50</h6>
-                <p className="card-text">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </p>
-                <button
-                  type="submit"
-                  className="btn btn-outline-secondary btn-sm"
-                >
-                  edit
-                </button>
-                <button type="submit" className="btn btn-outline-danger btn-sm">
-                  Delete
-                </button>
-              </div>
-            </div>
-          </div>
-          <div className="col">
-            <div className="card" style={{ width: "18rem" }}>
-              <div className="card-body">
-                <h5 className="card-title">Biriyani</h5>
-                <h6 className="card-subtitle mb-2 text-muted">$6.50</h6>
-                <p className="card-text">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </p>
-                <button
-                  type="submit"
-                  className="btn btn-outline-secondary btn-sm"
-                >
-                  edit
-                </button>
-                <button type="submit" className="btn btn-outline-danger btn-sm">
-                  Delete
-                </button>
-              </div>
-            </div>
-          </div>
+                </Button>
 
-          <div className="col">
-            <div className="card" style={{ width: "18rem" }}>
-              <div className="card-body">
-                <h5 className="card-title">Ice cream</h5>
-                <h6 className="card-subtitle mb-2 text-muted">Free</h6>
-                <p className="card-text">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </p>
-                <button
-                  type="submit"
-                  className="btn btn-outline-secondary btn-sm"
-                >
-                  edit
-                </button>
-                <button type="submit" className="btn btn-outline-danger btn-sm">
-                  Delete
-                </button>
-              </div>
-            </div>
+                {/* <Card.Link href="#">Another Link</Card.Link> */}
+              </Card.Body>
+            </Card>
           </div>
           <div className="col">
-            <div className="card" style={{ width: "18rem" }}>
-              <div className="card-body">
-                <h5 className="card-title">Water</h5>
-                <h6 className="card-subtitle mb-2 text-muted">$200</h6>
-                <p className="card-text">
+            <Card className="menu-item" style={{ width: "18rem" }}>
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Subtitle className="mb-2 text-muted">
+                  Card Subtitle
+                </Card.Subtitle>
+                <Card.Text>
                   Some quick example text to build on the card title and make up
                   the bulk of the card's content.
-                </p>
-                <button
-                  type="submit"
-                  className="btn btn-outline-secondary btn-sm"
-                >
-                  edit
-                </button>
-                <button type="submit" className="btn btn-outline-danger btn-sm">
+                </Card.Text>
+                <Button className="ms-1" variant="outline-secondary">
+                  Edit
+                </Button>
+                <Button className="ms-1" variant="outline-danger">
                   Delete
-                </button>
-              </div>
-            </div>
+                </Button>
+
+                {/* <Card.Link href="#">Another Link</Card.Link> */}
+              </Card.Body>
+            </Card>
+          </div>
+          <div className="col">
+            <Card className="menu-item" style={{ width: "18rem" }}>
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Subtitle className="mb-2 text-muted">
+                  Card Subtitle
+                </Card.Subtitle>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </Card.Text>
+                <Button className="ms-1" variant="outline-secondary">
+                  Edit
+                </Button>
+                <Button className="ms-1" variant="outline-danger">
+                  Delete
+                </Button>
+
+                {/* <Card.Link href="#">Another Link</Card.Link> */}
+              </Card.Body>
+            </Card>
           </div>
         </div>
       </div>
@@ -120,36 +139,65 @@ const AddEditMenu = () => {
       <hr />
       <form className="menu-form">
         <div className="form-group">
-          <label htmlFor="inputName">Item Name</label>
+          {formik.errors.itemName && formik.touched.itemName ? (
+            <div className="alert alert-danger mt-3 mb-1" role="alert">
+              {formik.errors.itemName}
+            </div>
+          ) : null}
+          <label className="add-menu-req" htmlFor="inputName">
+            Item name
+          </label>
           <input
+            name="itemName"
             type="text"
             className="form-control"
-            id="itemName"
-            placeholder="Enter item name"
+            id="item-name"
+            placeholder="Item Name"
+            {...formik.getFieldProps("itemName")}
           />
         </div>
-        <div className="form-group">
-          <label htmlFor="inputPrice">Price</label>
+
+        <div className="form-group mt-2">
+          {formik.errors.price && formik.touched.price ? (
+            <div className="alert alert-danger mt-3 mb-1" role="alert">
+              {formik.errors.price}
+            </div>
+          ) : null}
+          <label className="add-menu-req" htmlFor="inputPrice">
+            Price
+          </label>
           <input
+            name="price"
             type="number"
             className="form-control"
-            id="inputPrice"
+            id="menu-price"
             placeholder="Price"
+            {...formik.getFieldProps("price")}
           />
         </div>
-        <div className="form-group">
-          <label htmlFor="inputDescription">Description</label>
+
+        {formik.errors.description && formik.touched.description ? (
+          <div className="alert alert-danger mt-3 mb-1" role="alert">
+            {formik.errors.description}
+          </div>
+        ) : null}
+        <div className="form-group mt-2">
+          <label className="add-menu-req" htmlFor="inputDescription">
+            Description
+          </label>
           <textarea
+            name="description"
             className="form-control"
-            id="inputDescription"
-            placeholder="Enter Description"
+            id="menu-description"
+            placeholder="Description"
             cols="20"
             rows="5"
+            {...formik.getFieldProps("description")}
           ></textarea>
         </div>
-        <div className="text-center mt-2">
+        <div className="text-center mt-3">
           <button type="submit" className="btn btn-success">
-            Add Menu Item
+            Add/Edit Menu Item
           </button>
         </div>
       </form>
